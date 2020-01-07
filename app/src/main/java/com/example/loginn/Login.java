59893ab40e1fbd5623aa55444163a235de57fda6
MuhammadDandriHandian <mdhandian@gmail.com>
2020-01-07 19:15:02 +0700

    push 1

---
+++ app/src/main/java/com/example/loginn/Login.java
@@ -0,0 +1,17 @@
+package com.example.loginn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
